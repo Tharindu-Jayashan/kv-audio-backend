c19856bfc8d67995abe4f8498f4c776d40464e79
Tharindu Jayashan <tharindujayashan421@gmail.com>
2025-01-14 09:25:22 +0530

    update product model

--- models/product.js
+++ models/product.js
@@ -2,17 +2,45 @@ import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
 
+    key : {
+        type : String,
+        unique : true,
+        required : true 
+    },
+
     name : {
         type : String,
         required : true
     },
 
+    category : {
+        type : String,
+        required : true,
+        default : "uncategorized"
+    },
+
+    dimensions : {
+        type : String,
+        required : true
+    },
+    
     price : {
         type : Number,
         required : true
     },
 
     description : {
+        type : String,
+        required : true
+    },
+    
+    availability : {
+        type : Boolean,
+        required : true,
+        default : true
+    },
+    
+    image : {
         type : String,
         required : true
     }
