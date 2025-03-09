d1698429c82f48e968ed1e6fd96725e4089676e7
Tharindu Jayashan <tharindujayashan421@gmail.com>
2025-03-09 20:04:51 +0530

    implement getQuote function in OrderController

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -95,3 +95,65 @@ export async function createOrder(req,res){
         console.log(error);
     }
 }
+
+export async function getQuote(req, res) {
+    console.log(req.body)
+	const data = req.body;
+	const orderInfo = {
+		orderedItems: [],
+	};
+	let oneDayCost = 0;
+	for (let i = 0; i < data.orderedItems.length; i++) {
+		try {
+			const product = await Product.findOne({ key: data.orderedItems[i].key });
+			if (product == null) {
+				res.status(404).json({
+					message:
+						"Product with key " + data.orderedItems[i].key + " not found",
+				});
+				return;
+			}
+			if (product.availability == false) {
+				res.status(400).json({
+					message:
+						"Product with key " +
+						data.orderedItems[i].key +
+						" is not available",
+				});
+				return;
+			}
+			orderInfo.orderedItems.push({
+				product: {
+					key: product.key,
+					name: product.name,
+					image: product.image[0],
+					price: product.price,
+				},
+				quantity: data.orderedItems[i].qty,
+			});
+
+			oneDayCost += product.price * data.orderedItems[i].qty;
+		} catch (e) {
+			res.status(500).json({
+				message: "Failed to create order",
+			});
+			return;
+		}
+	}
+
+	orderInfo.days = data.days;
+	orderInfo.startingDate = data.startingDate;
+	orderInfo.endingDate = data.endingDate;
+	orderInfo.totalAmount = oneDayCost * data.days;
+	try {
+		res.json({
+			message: "Order quatation",
+			total: orderInfo.totalAmount,
+		});
+	} catch (e) {
+		console.log(e);
+		res.status(500).json({
+			message: "Failed to create order",
+		});
+	}
+}
